35dfc6103a7110e50822691ec15d7fff9925722c
paolofullone <paolo.fullone@gmail.com>
2022-01-23 18:49:01 -0300

    atualização vercel

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     <div>
       <h1> Teste Felipe Deschamps Node JS</h1>
       <p>https://www.youtube.com/watch?v=EW7m2WIvFgQ</p>
+      <p>Muito legal essa Vercel, usarei com certeza.</p>
     </div>
   );
 }
